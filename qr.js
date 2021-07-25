fcac02ffb6ba2c0a36cc57c9d8f216d2e727436a
Thushan2003 <86910590+Thushan2003@users.noreply.github.com>
2021-07-25 16:41:25 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,44 +1,52 @@
-/* Copyright (C) 2021 Queen Amdi.
-
+/* Copyright (C) 2020 Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
-Queen Amdi - Black Amda
+WhatsAsena - Yusuf Usta
 */
 
-
 const chalk = require('chalk');
-const {WAConnection} = require('@adiwajshing/baileys');
+const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
 const {StringSession} = require('./whatsasena/');
 const fs = require('fs');
 
 async function whatsAsena () {
     const conn = new WAConnection();
     const Session = new StringSession();  
+    conn.version = [2, 2119, 6]
     conn.logger.level = 'warn';
-    conn.regenerateQRIntervalMs = 40000;
+    conn.regenerateQRIntervalMs = 50000;
     
-    conn.on('🔄 connecting', async () => {
-        console.log(`${chalk.green.bold('Queen')}${chalk.blue.bold(' Amdi')}
-${chalk.white.italic('Queen Amdi Sting session')}
-
-${chalk.blue.italic('ℹ️  Whatsapp වෙත සම්බන්ධ වෙමින්... කරුණාකර රැඳී සිටින්න.')}`);
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
+${chalk.white.italic('AsenaString Kodu Alıcı')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     });
     
 
-    conn.on('open', () => {
+    conn.on('open', async () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('Queen Amdi session: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `AMDI_SESSION="${st}"`);
+            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
         }
-
-        console.log(
-            chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js.')
-        );
+        if (conn.user.jid.startsWith('90')) {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Bu Kodu Kimseyle Paylaşmayın!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('Locale kuruyorsanız node bot.js ile botu başlatabilirsiniz.')
+            );
+        }
+        else {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
+            );
+        }
+        
         process.exit(0);
     });
 
